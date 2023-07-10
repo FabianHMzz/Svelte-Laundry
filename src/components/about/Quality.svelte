2e14c48803ec668368da112336b495519d62d78c
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-09 23:01:07 -0600

    ✨ Quality component finished

---
+++ src/components/about/Quality.svelte
@@ -0,0 +1,26 @@
+<section>
+  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+
+    <div class="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-first lg:h-full">
+        <img
+          alt="Vision"
+          src="https://images.unsplash.com/photo-1549037173-e3b717902c57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-16">
+        <article class="space-y-4 text-gray-600">
+          <h2 class="text-3xl font-[PoppinsBold] text-[#0F74BA]  sm:text-4xl">Política de Calidad</h2>
+          <p class="mt-4 font-[Satoshi] text-gray-800">
+            En nuestra lavandería, nos comprometemos a brindar un servicio excepcional y cuidar de la ropa de nuestros clientes de manera óptima.
+             Nos esforzamos por cumplir con los estándares de calidad, mejorar continuamente nuestros procesos y operar de manera sostenible. 
+             Valoramos a nuestro equipo y su desarrollo profesional. Nuestro objetivo es garantizar la satisfacción de nuestros clientes y mantener 
+             un ambiente seguro y limpio en nuestras instalaciones.
+          </p>
+        </article>
+      </div>
+    </div>
+  </div>
+</section>
