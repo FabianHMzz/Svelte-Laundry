7a5f2b21ac6838870fb1cd2924908f9999368e26
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-09 23:00:51 -0600

    ✨ Mission component finished

---
+++ src/components/about/Mission.svelte
@@ -0,0 +1,28 @@
+<section>
+  <div
+    class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+  >
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div
+        class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
+      >
+        <img
+          alt="Mision"
+          src="https://images.unsplash.com/photo-1635369306367-9d891c6c1a7c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-24">
+        <h2 class="text-3xl font-[PoppinsBold] text-[#0F74BA]  sm:text-4xl">Misión</h2>
+
+        <p class="mt-4 font-[Satoshi] text-gray-800">
+          Nuestra misión es proporcionar un servicio de lavado de ropa excepcional y conveniente para nuestros clientes. 
+          Nos esforzamos por cuidar de su ropa de manera eficiente y brindarles una experiencia satisfactoria. Nos comprometemos a 
+          utilizar tecnología avanzada y prácticas sostenibles para minimizar nuestro impacto en el medio ambiente. Nuestra meta es simplificar el 
+          cuidado de la ropa y mejorar la vida de las personas a través de nuestro servicio confiable y amigable.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
