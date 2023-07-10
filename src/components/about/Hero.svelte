f4fc7b406536876e2c6dfc66e929f421fcd284b8
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-09 22:59:53 -0600

    🛠️ breadcrum link reapred

--- src/components/about/Hero.svelte
+++ src/components/about/Hero.svelte
@@ -23,7 +23,7 @@
                   <li>
                     <div class="flex items-center">
                       <svg aria-hidden="true" class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                      <a href="/about-us" class="ml-1 text-sm font-medium text-white hover:text-[#e1e1e1] shadow-black drop-shadow-md md:ml-2">Acerca de nosotros</a>
+                      <a href="/about" class="ml-1 text-sm font-medium text-white hover:text-[#e1e1e1] shadow-black drop-shadow-md md:ml-2">Acerca de nosotros</a>
                     </div>
                   </li>
                 </ul>
