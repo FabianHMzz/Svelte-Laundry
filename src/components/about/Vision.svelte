96331a8a6deb4475da4408473e17f7505c825178
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-09 23:01:48 -0600

    ✨ Vision component finished

---
+++ src/components/about/Vision.svelte
@@ -0,0 +1,26 @@
+<section>
+  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+
+    <div class="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-first lg:h-full">
+        <img
+          alt="Vision"
+          src="https://images.unsplash.com/photo-1508759073847-9ca702cec7d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-16">
+        <article class="space-y-4 text-gray-600">
+          <h2 class="text-3xl font-[PoppinsBold] text-[#0F74BA]  sm:text-4xl">Visión</h2>
+          <p class="mt-4 font-[Satoshi] text-gray-800">
+            Nuestra lavandería se esfuerza por brindar a los clientes una experiencia de lavado y secado de ropa rápida, conveniente y de calidad. 
+            Nos destacamos por nuestra tecnología avanzada, enfoque en la sostenibilidad y servicio excepcional. Queremos simplificar la vida de las 
+            personas al ofrecer un lugar limpio y acogedor, con máquinas intuitivas y amigables. Nuestro objetivo es ser la lavandería preferida, donde los 
+            clientes confíen en que su ropa será cuidada de manera eficiente y respetuosa con el medio ambiente.
+          </p>
+        </article>
+      </div>
+    </div>
+  </div>
+</section>
