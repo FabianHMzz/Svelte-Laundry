a26a8e574003926d164a4ea6a93ffe504ee5c277
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-09 23:01:21 -0600

    ✨ Values component finished

---
+++ src/components/about/Values.svelte
@@ -0,0 +1,45 @@
+<section>
+  <div
+    class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+  >
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div
+        class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
+      >
+        <img
+          alt="Mision"
+          src="https://images.unsplash.com/photo-1650964336783-fd8c0c241b13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-24">
+        <h2 class="text-3xl font-[PoppinsBold] text-[#0F74BA]  sm:text-4xl">Valores Organizacionales</h2>
+
+        <ul>
+          <li class="font-[PoppinsMedium] text-lg py-4">1.- Calidad
+            <p class="font-[Satoshi] mt-2">
+              Nos comprometemos a ofrecer un servicio de calidad excepcional en todas nuestras operaciones. 
+              Nos esforzamos por cuidar de la ropa de nuestros clientes con atención meticulosa y garantizar 
+              resultados impecables en cada lavado.
+            </p>
+          </li>
+          <li class="font-[PoppinsMedium] text-lg py-4">2.- Confiabilidad
+            <p class="font-[Satoshi] mt-2">
+              Valoramos la confianza de nuestros clientes y nos esforzamos por ser un servicio en el que puedan confiar plenamente. 
+              Cumplimos con nuestros compromisos, ofreciendo tiempos de entrega confiables y garantizando la seguridad y el cuidado
+               de la ropa de nuestros clientes.
+            </p>
+          </li>
+          <li class="font-[PoppinsMedium] text-lg py-4">3.- Responsabilidad
+            <p class="font-[Satoshi] mt-2">
+              Reconocemos nuestra responsabilidad hacia nuestros clientes, empleados y el medio ambiente. 
+              Nos comprometemos a operar de manera ética y sostenible, minimizando nuestro impacto en el entorno 
+              y tomando decisiones responsables en todas nuestras actividades.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
