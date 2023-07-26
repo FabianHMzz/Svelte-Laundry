ce327b100378c0bc0821d0b6894265af674d7563
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-25 23:54:51 -0600

    🔁some tailwind classes changed

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -69,7 +69,7 @@
               <button
                 type='submit'
                 value='Send'
-                class='flex mx-auto border-2 border-[#0F74BA] px-5 py-2 rounded-full select-none hover:bg-[#0F74BA] hover:text-white transition duration-300 ease-in-out text-[#0F74BA] font-[PoppinsBold]'
+                class='flex mx-auto border-2 border-[#0F74BA] px-5 py-2 rounded-full select-none hover:bg-[#0F74BA] hover:text-white transition-colors duration-300 ease-in-out text-[#0F74BA] font-[PoppinsBold]'
               >
                 Enviar Mensaje
               </button>
